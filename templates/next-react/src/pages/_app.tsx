4978b72a6c24381b83d4a8726a84659313acb723
Guillaume CATEL <hello@guillaumecatel.com>
2022-04-03 15:09:01 +0200

    feat(template/next-react): add sass

--- templates/next-react/src/pages/_app.tsx
+++ templates/next-react/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
