c5fe2b79d3e4a62b29c67747cdac7e36f3b9a2d5
Guillaume CATEL <hello@guillaumecatel.com>
2022-04-03 15:15:00 +0200

    feat(template/next-react): add @guillaumecatel/css-reset

--- templates/next-react/src/pages/_app.tsx
+++ templates/next-react/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
+import '@guillaumecatel/css-reset'
+
 import '@/styles/globals.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
