da794908506717b4a76660c4dc48fd724ffbfcc9
Guillaume CATEL <hello@guillaumecatel.com>
2022-04-03 14:27:06 +0200

    feat(template/next-react): remove .next folder

--- templates/next-react/.next/static/chunks/webpack.js
+++ templates/next-react/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e84407138d4f4363"; }
+/******/ 		__webpack_require__.h = function() { return "73b49e3c78fe680b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
